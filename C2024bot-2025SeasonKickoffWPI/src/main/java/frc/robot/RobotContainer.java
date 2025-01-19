26bcf9a8e0ff2c2b01d3a4e283441d59fca34cef
Samyoppp <sammycolewilson@gmail.com>
2025-01-19 15:30:28 -0500

    GFagra

--- C2024bot-2025SeasonKickoffWPI/src/main/java/frc/robot/RobotContainer.java
+++ C2024bot-2025SeasonKickoffWPI/src/main/java/frc/robot/RobotContainer.java
@@ -35,6 +35,8 @@ import edu.wpi.first.wpilibj2.command.button.Trigger;
 import edu.wpi.first.wpilibj2.command.sysid.SysIdRoutine;
 
 /**
+ * 
+ * TESTTTTT
  * This class is where the bulk of the robot should be declared. Since Command-based is a
  * "declarative" paradigm, very little robot logic should actually be handled in the {@link Robot}
  * periodic methods (other than the scheduler calls). Instead, the structure of the robot (including
