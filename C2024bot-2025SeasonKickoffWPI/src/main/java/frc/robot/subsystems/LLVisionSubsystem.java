ce83d7a67a4055b3ad6168d12e796e63c3100d39
Jordan Wilson <jordanwilson@hotmail.com>
2025-02-09 23:15:59 -0500

    Added April Tag Coordinates as Comment in Limelight Subsystem

--- C2024bot-2025SeasonKickoffWPI/src/main/java/frc/robot/subsystems/LLVisionSubsystem.java
+++ C2024bot-2025SeasonKickoffWPI/src/main/java/frc/robot/subsystems/LLVisionSubsystem.java
@@ -26,6 +26,36 @@ public class LLVisionSubsystem extends SubsystemBase implements VisionHelpers {
    * AprilTag Vision Subsystem based on LimeLight.
    * The coordinate tracking is always done with 0,0 on the lower blue side of the field.
   */
+
+/*
+
+April Tag Coordinates
+
+ID X       Y        Z       Z-Rot  Y-Rot
+1 657.37   25.80    58.50   126    0
+2 657.37   291.20   58.50   234    0
+3 455.15   317.15   51.25   270    0
+4 365.20   241.64   73.54   0      30
+5 365.20   75.39    73.54   0      30
+6 530.49   130.17   12.13   300    0
+7 546.87   158.50   12.13   0      0
+8 530.49   186.83   12.13   60     0
+9 497.77   186.83   12.13   120    0
+10 481.39  158.50   12.13   180    0
+11 497.77  130.17   12.13   240    0
+12 33.51   25.80    58.50   54     0
+13 33.51   291.20   58.50   306    0
+14 325.68  241.64   73.54   180    30
+15 325.68  75.39    73.54   180    30
+16 235.73  -0.15    51.25   90     0
+17 160.39  130.17   12.13   240    0
+18 144.00  158.50   12.13   180    0
+19 160.39  186.83   12.13   120    0
+20 193.10  186.83   12.13   60     0
+21 209.49  158.50   12.13   0      0
+22 193.10  130.17   12.13   300    0
+*/
+
   public LLVisionSubsystem() {}
 
   public Pose2d getRobotFieldPoseLL() {
